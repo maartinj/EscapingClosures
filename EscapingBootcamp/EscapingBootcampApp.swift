1320fe316a956b0cdc1ad2571fda9040d7a1296c
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-18 23:23:22 +0200

    first commit: Load initial files

---
+++ EscapingBootcamp/EscapingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapingBootcampApp.swift
+//  EscapingBootcamp
+//
+//  Created by Marcin Jędrzejak on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct EscapingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
